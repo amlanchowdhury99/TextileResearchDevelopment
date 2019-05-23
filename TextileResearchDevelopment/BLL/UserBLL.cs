e829fd3e2559794a41d3eb845d3b8edcf119b2f7
amlan <amlanchowdhury99@gmail.com>
2019-05-23 18:25:41 +0600

    Authorization1

---
+++ TextileResearchDevelopment/BLL/UserBLL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TextileResearchDevelopment.Models;
+using TextileResearchDevelopment.DataAccessLayer;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace TextileResearchDevelopment.BLL
+{
+    public class UserBLL
+    {
+        static string connectionStr = DBGateway.connectionString;
+
+        internal static bool IsUserValid(User user)
+        {
+            try
+            {
+                if(DBGateway.recordExist("SELECT * FROM UserInfo WHERE UserName = '"+user.UserName+"' AND Password = '" + user.Password + "'"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
--- TextileResearchDevelopment/Controllers/HomeController.cs
+++ TextileResearchDevelopment/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using TextileResearchDevelopment.Models;
 using TextileResearchDevelopment.BLL;
+using System.Web.SessionState;
 
 namespace TextileResearchDevelopment.Controllers
 {
@@ -13,7 +14,9 @@ namespace TextileResearchDevelopment.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            if(Session["UserName"] != null)
+            //HttpSessionState sessionValue = System.Web.HttpContext.Current.Session;
+
+            if (Session[System.Web.HttpContext.Current.Session.SessionID] != null)
             {
                 return View();
             }
@@ -26,7 +29,7 @@ namespace TextileResearchDevelopment.Controllers
         [HttpGet]
         public ActionResult LoginForm()
         {
-            if (Session["UserName"] == null)
+            if (Session[System.Web.HttpContext.Current.Session.SessionID] == null)
             {
                 return View();
             }
@@ -41,20 +44,20 @@ namespace TextileResearchDevelopment.Controllers
         [ValidateAntiForgeryToken]
         public JsonResult CheckAuthentication(User user)
         {
-            // Your code to check the Authentication
-            //int count = 1;
-            //if (count == 1)
-            //{
-            //    return Json("Authenticated", JsonRequestBehavior.AllowGet);
-            //}
-            //else
-            //{
-            //    return Json("Failed", JsonRequestBehavior.AllowGet);
-            //}
+            if (UserBLL.IsUserValid(user))
+            {
+                //Session[user.UserName] = user.Password;
+                Session[System.Web.HttpContext.Current.Session.SessionID] = user.UserName;
+                return Json("Authenticated", JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json("Failed", JsonRequestBehavior.AllowGet);
+            }
 
-            Session["UserName"] = "b";
+            //Session["UserName"] = "b";
 
-            return Json(new { success = true, redirecturl = @Url.Action("Index") });
+            //return Json(new { success = true, redirecturl = @Url.Action("Index") });
         }
         public ActionResult About()
         {
